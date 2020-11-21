cdc662d86e393995e05220219908914a7057c6a8
redpulse96 <impulse96@icloud.com>
2020-11-21 22:03:04 +0530

    apis added

--- src/driver/db.js
+++ src/driver/db.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const { db } = require('./model');
 
 mongoose.connect('mongodb+srv://admin:Daressalam@defconone-ak0ki.mongodb.net/Defcon_One_Auth?retryWrites=true&w=majority', {
   useUnifiedTopology: true,
